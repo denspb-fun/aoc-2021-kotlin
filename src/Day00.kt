63aad66bc9ffe3fd9ea7216ffdc33f01e19e3c9f
Denis K <95908588+denspb-fun@users.noreply.github.com>
2021-12-20 11:49:10 +0100

    Day 01 + some structuring

---
+++ src/Day00.kt
@@ -0,0 +1,20 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 1
+    }
+
+    fun part2(input: List<String>): Int {
+        return 2
+    }
+
+    val day = "01"
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day" + day +"_test")
+//    check(part1(testInput) == 1)
+//    check(part2(testInput) == 2)
+
+    val input = readInput("Day+ " + day)
+    println(part1(input))
+    println(part2(input))
+}
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val pairs = input.map { it.toInt() }.zipWithNext()
+        return pairs.count { (x, y) -> y > x }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+       val windowed = input.map { it.toInt() }.windowed(3).map { it.sum() }
+       val pairs = windowed.zipWithNext()
+
+        return pairs.count { (x, y) -> y > x }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
